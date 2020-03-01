49850f5f19dce8971ca0d36c9819a153659794d6
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-01 13:55:14 -0600

    Super sweet title screen animations

--- main_menu.gd
+++ main_menu.gd
@@ -1,16 +1,29 @@
 extends Control
 
+const FADE_TIME = 1.0
+onready var black_flash = get_node("BlackFlash")
+var fade_progress = 0.0
+signal fade_finished # for function callback
 
 func _ready():
-#	get_node("score").set_text("HIGH SCORE: " + str(get_node("/root/game_state").max_points))
 	set_process(true)
 
-
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
-		_on_play_pressed()
-
+	if not get_tree().get_root().is_input_disabled():
+		if Input.is_action_pressed("ui_accept"):
+			get_node("NewGame")._on_button_pressed()
+	else:
+		if fade_progress >= FADE_TIME:
+			emit_signal("fade_finished")
+			set_process(false)
+		else:
+			fade_progress += delta
+			black_flash.set_modulate(Color(0.0, 0.0, 0.0, fade_progress))
 
 func _on_play_pressed():
-#	get_node("/root/game_state").points = 0
+	black_flash.show()
+	black_flash.set_modulate(Color(0.0, 0.0, 0.0, 0.0))
+	connect("fade_finished", self, "new_game")
+
+func new_game():
 	get_tree().change_scene("res://scenes/levels/lvl0.tscn")
---
+++ scripts/button.gd
@@ -0,0 +1,31 @@
+extends Button
+
+var white_flash # ../WhiteFlash
+const FLASH_DUR = 0.3
+var times = 0
+var alpha = 0.0
+var progress = 0.0 # out of 1.0
+var raising = true # whether to add or subtract from alpha
+
+func _process(delta):
+	if times >= 2: # if we finished the animation
+		set_process(false)
+		get_parent()._on_play_pressed()
+		return
+	
+	if raising: progress += delta / (FLASH_DUR / 2)
+	else:       progress -= delta / (FLASH_DUR / 2)
+	alpha = min(progress * progress, 1.0) # y=x^2
+	
+	if alpha >= 1.0: raising = false
+	elif alpha <= 0.1 and raising == false: raising = true; times += 1
+	white_flash.set_modulate(Color(1.0, 1.0, 1.0, alpha))
+
+func _on_button_pressed():
+	set_process(true)
+	get_tree().get_root().set_disable_input(true) # prevent input while doing animation
+	
+	white_flash = get_parent().get_node("WhiteFlash")
+	white_flash.show() # make it visible
+	white_flash.edit_set_rect(get_rect()) # fit it over this button
+	white_flash.set_modulate(Color(1.0, 1.0, 1.0, 0.0))
