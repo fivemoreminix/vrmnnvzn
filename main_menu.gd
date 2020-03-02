0a3d49aef8b8f5ee0821d60e394d797591399b9a
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-01 19:28:13 -0600

    Fade abstraction

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,6 @@
 extends Control
 
-const FADE_TIME = 1.0
-onready var black_flash = get_node("BlackFlash")
-var fade_progress = 0.0
-signal fade_finished # for function callback
+onready var fade = get_node("Fade")
 
 func _ready():
 	set_process(true)
@@ -14,19 +11,11 @@ func _process(delta):
 	if not get_tree().get_root().is_input_disabled():
 		if Input.is_action_pressed("ui_accept"):
 			get_node("NewGame")._on_button_pressed()
-	else:
-		if fade_progress >= FADE_TIME:
-			emit_signal("fade_finished")
-			get_tree().get_root().set_disable_input(false)
-		else:
-			fade_progress += delta
-			black_flash.set_modulate(Color(0.0, 0.0, 0.0, fade_progress))
 
 func handle(func_name):
-	black_flash.show()
-	black_flash.set_modulate(Color(0.0, 0.0, 0.0, 0.0))
-	print(get_signal_connection_list("fade_finished"))
-	connect("fade_finished", self, func_name)
+	fade.begin_fade_out()
+	print(fade.get_signal_connection_list("fade_finished"))
+	fade.connect("fade_finished", self, func_name)
 
 func new_game():
 	get_tree().change_scene("res://new_game.tscn")
@@ -37,3 +26,6 @@ func continue_game():
 	var index = f.get_line()
 	print("opened save file and got index = " + index)
 	get_tree().change_scene("res://scenes/levels/lvl" + index + ".tscn")
+
+func _on_fade_finished():
+	get_tree().get_root().set_disable_input(false)
---
+++ scripts/fade.gd
@@ -0,0 +1,21 @@
+extends Node
+
+signal fade_finished # for function callback
+
+const FADE_TIME = 1.0
+onready var black_flash = get_node("BlackFlash")
+var fade_progress = 0.0
+
+func _process(delta):
+	if fade_progress >= FADE_TIME:
+		emit_signal("fade_finished")
+		get_tree().get_root().set_disable_input(false)
+		set_process(false)
+	else:
+		fade_progress += delta
+		black_flash.set_modulate(Color(0.0, 0.0, 0.0, fade_progress))
+
+func begin_fade_out():
+	black_flash.show()
+	black_flash.set_modulate(Color(0.0, 0.0, 0.0, 0.0))
+	set_process(true)
