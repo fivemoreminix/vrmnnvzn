a5b6e9e2c298bf5d7706ad473753c35d0b4d1d5f
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-01 23:47:30 -0600

    Scrolling text system

---
+++ bar.gd
@@ -0,0 +1,27 @@
+extends Panel
+
+const SCROLLING_SPEED = 135
+
+onready var scrol_lbl = get_node("ScrollingText")
+onready var scrol_lbl_default_x = scrol_lbl.get_pos().x
+
+var _scrolling_text = false
+
+func _ready():
+	scrolling_text("Woah, that's a big invasion ahead of you! Stay frosty.")
+
+func _process(delta):
+	if _scrolling_text:
+		var cur_pos = scrol_lbl.get_pos()
+		print(cur_pos)
+		scrol_lbl.set_pos(Vector2(cur_pos.x-delta*SCROLLING_SPEED, cur_pos.y))
+		if cur_pos.x + scrol_lbl.get_size().x < 0: # if out of frame ...
+			scrol_lbl.set_pos(Vector2(scrol_lbl_default_x, cur_pos.y))
+			scrol_lbl.hide()
+			_scrolling_text = false
+
+func scrolling_text(text):
+	scrol_lbl.show()
+	scrol_lbl.set_text(text)
+	_scrolling_text = true
+	set_process(true)
