9640c37cba3a8344c4d9f575c53f66f84dc39b55
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-01 18:42:39 -0600

    Warning for overwriting saved games

--- main_menu.gd
+++ main_menu.gd
@@ -7,6 +7,8 @@ signal fade_finished # for function callback
 
 func _ready():
 	set_process(true)
+	
+	get_node("Continue").set_disabled(not File.new().file_exists("user://VRMNNVZN.save"))
 
 func _process(delta):
 	if not get_tree().get_root().is_input_disabled():
@@ -23,7 +25,15 @@ func _process(delta):
 func handle(func_name):
 	black_flash.show()
 	black_flash.set_modulate(Color(0.0, 0.0, 0.0, 0.0))
+	print(get_signal_connection_list("fade_finished"))
 	connect("fade_finished", self, func_name)
 
 func new_game():
 	get_tree().change_scene("res://new_game.tscn")
+
+func continue_game():
+	var f = File.new()
+	f.open("user://VRMNNVZN.save", File.READ)
+	var index = f.get_line()
+	print("opened save file and got index = " + index)
+	get_tree().change_scene("res://scenes/levels/lvl" + index + ".tscn")
--- new_game.gd
+++ new_game.gd
@@ -1,12 +1,19 @@
 extends Control
 
 func _ready():
+	# these are hidden by default
+	get_node("Warning").hide()
+	get_node("MainDialog").hide()
+	
 	var f = File.new()
 	if f.file_exists("user://VRMNNVZN.save"): # if a save file exists ...
 		get_node("Warning").show()
 	else:
-		get_node("MainDialog").show()
-
+		# TODO: difficulty selection
+#		get_node("MainDialog").show()
+		f.open("user://VRMNNVZN.save", File.WRITE)
+		f.store_string("0") # store level number
+		get_tree().change_scene("res://scenes/levels/lvl0.tscn")
 
 func _on_Warning_user_made_decision(decision): # decision true if continue, false if no
 	print("user made decision = " + str(decision))
@@ -14,4 +21,5 @@ func _on_Warning_user_made_decision(decision): # decision true if continue, fals
 		get_tree().change_scene("res://main_menu.tscn")
 	else:
 		get_node("Warning").hide()
-		get_node("MainDialog").show()
+		get_tree().change_scene("res://scenes/levels/lvl0.tscn")
+#		get_node("MainDialog").show()
