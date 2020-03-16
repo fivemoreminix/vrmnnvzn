8b4d69163abe8ed5200afad98b4a3dc2ad549285
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-16 00:54:25 -0500

    Creating a new game actually creates game data

--- game_data.gd
+++ game_data.gd
@@ -5,10 +5,30 @@ var data = null
 var global_data = null
 
 func save_data(): # save game data to disk
-	pass
+	var f = File.new()
+	f.open("user://VRMNNVZN.save", File.WRITE)
+	f.store_string(data.to_json())
+	f.close()
 
 func load_data(): # load saved game data from disk
-	pass
+	var f = File.new()
+	if f.file_exists("user://VRMNNVZN.save"): # if a save file exists ...
+		f.open("user://VRMNNVZN.save", File.READ)
+		data = {} # give a value so .parse_json doesn't cry...
+		var err = data.parse_json(f.get_as_text())
+		f.close()
+		if err != OK:
+			printerr("game_data.gd: failed to parse game data save file")
+		return err
+#	else: # data save file does NOT exist ...
+#		data = { ### data defaults ###
+#			current_level: 0,
+#			highest_level_discovered: 0,
+#			name: "", # player's name
+#		}
+#		print("game_data.gd: game data save file not found... creating one now")
+#		return save_data() # make the file
+	return ERR_FILE_NOT_FOUND
 
 func save_global_data(): # save global data to disk
 	var f = File.new()
--- new_game.gd
+++ new_game.gd
@@ -12,26 +12,26 @@ func _ready():
 		get_node("Warning").show()
 	else:
 		# TODO: difficulty selection
-#		get_node("MainDialog").show()
-		f.open("user://VRMNNVZN.save", File.WRITE)
-		f.store_string("0") # store level number
-		begin_fade_and_load_first_level()
+		get_node("MainDialog").show()
+#		begin_fade_and_load_first_level()
 
 func _on_Warning_user_made_decision(decision): # decision true if continue, false if no
 	print("user made decision = " + str(decision))
 	if decision == false:
+		fade.connect("fade_finished", self, "_on_fade_finished")
 		fade.begin_fade_out()
 	else:
-#		get_node("Warning").hide()
-		begin_fade_and_load_first_level()
-#		get_node("MainDialog").show()
+		get_node("Warning").hide()
+#		begin_fade_and_load_first_level()
+		get_node("MainDialog").show()
+		get_node("MainDialog/Name").grab_focus()
 
-func begin_fade_and_load_first_level():
-	fade.begin_fade_out()
-	fade.connect("fade_finished", self, "_on_fade_finished_but_load_first_level")
-
-func _on_fade_finished_but_load_first_level():
-	get_tree().change_scene("res://scenes/levels/lvl0.tscn")
+#func begin_fade_and_load_first_level():
+#	fade.begin_fade_out()
+#	fade.connect("fade_finished", self, "_on_fade_finished_but_load_first_level")
+#
+#func _on_fade_finished_but_load_first_level():
+#	get_tree().change_scene("res://scenes/levels/lvl0.tscn")
 
 func _on_fade_finished():
 	get_tree().change_scene("res://main_menu.scn")
