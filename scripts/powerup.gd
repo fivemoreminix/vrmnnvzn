1e907722a2a0cae82f2d6b0f9d35ef39e4823305
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-17 18:36:50 -0500

    Random powerups

--- scripts/powerup.gd
+++ scripts/powerup.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
 # TODO: make this a tool script and change sprite based upon which type
-export(String, "Fast shooting", "Phase-through", "Triple-shot", "Shoot diag") var powerup = "Fast shooting"
+export(String, "Random", "Fast shooting", "Phase-through", "Triple-shot", "Shoot diag") var powerup = "Random"
 
 func _on_Powerup_area_enter( area ):
 	if area.get_name() == "ship":
 		area.add_effect(powerup)
 		queue_free() # Remove this powerup from scene
+
+func _ready():
+	if powerup == "Random":
+		var n = randi() % 4
+		if   n == 0: powerup = "Fast shooting"
+		elif n == 1: powerup = "Phase-through"
+		elif n == 2: powerup = "Triple-shot"
+		elif n == 3: powerup = "Shoot diag"
