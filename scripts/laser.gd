20e112c506639a21c6b36eb7b9ae5585eb9c84e0
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-17 16:36:04 -0500

    Laser enemy but it has some issues

---
+++ scripts/laser.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+func _ready():
+	expand_beam()
+	get_node("TimerWhileOff").start()
+
+func expand_beam():
+	var scale = Vector2(get_node("RightHead").get_pos().x-5, 1)
+	get_node("LeftHead/Beam/Sprite").set_scale(scale)
+	get_node("LeftHead/Beam/CollisionShape2D").set_scale(scale)
+	get_node("LeftHead/Beam/CollisionShape2D").set_pos(Vector2(scale.x/2, scale.y)) # colliders scale from middle, so we offset it here
+
+
+func _on_VisibilityEnabler2D_exit_screen():
+	print(get_name() + " has left the screen... freeing self :'(")
+	queue_free()
+
+
+func _on_TimerWhileOff_timeout(): # After being off ...
+	# Begin playing "charging" animation
+	# Start charging timer
+	
+	# TODO: anim...
+	get_node("TimerWhileCharging").start()
+
+func _on_TimerWhileCharging_timeout(): # After fully charging ...
+	# show beam
+	# enable beam collisions
+	
+	get_node("LeftHead/Beam").show()
+	get_node("LeftHead/Beam").set_monitorable(true)
+	
+	get_node("TimerWhileOn").start()
+
+func _on_TimerWhileOn_timeout(): # After being on ...
+	# stop showing the beam
+	# disable beam collisions
+	
+	get_node("LeftHead/Beam").hide()
+	get_node("LeftHead/Beam").set_monitorable(false) # Collider cannot be seen by other areas
+	
+	get_node("TimerWhileOff").start()
