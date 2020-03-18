0068078909edf4baefe5ca9c45153449aeba7973
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-17 22:11:50 -0500

    Fixed button flasher

--- scripts/button_flasher.gd
+++ scripts/button_flasher.gd
@@ -4,17 +4,18 @@ signal flash_finished
 
 func flash(button_rect):
 	show() # make self visible
-	get_node("White").show()
 	get_tree().get_root().set_disable_input(true) # prevent input while doing animation
-	get_node("White").edit_set_rect(button_rect)
+	get_node("White").set_global_pos(button_rect.pos)
+	get_node("White").set_size(button_rect.size)
 	get_node("Animator").play("flash")
-	print(get_node("Animator").get_pos())
 
 
 func _on_Animator_finished():
 	# reset state
 	hide()
 	get_tree().get_root().set_disable_input(false)
-	get_node("Animator").seek(0)
 	
 	emit_signal("flash_finished")
+	
+	for conn in get_signal_connection_list("flash_finished"): # Clear every connection to this signal after emitting it
+		disconnect(conn["signal"], conn.target, conn.method)
