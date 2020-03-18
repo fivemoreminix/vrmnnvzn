c5606ed6daefe797a614268bc3f119e63525022c
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-18 12:57:24 -0500

    Replace old pinsirs and delete unused junk

--- scripts/bee.gd
+++ scripts/bee.gd
@@ -2,21 +2,24 @@
 extends Area2D
 
 # Member variables
-const SPEED    = 60
-const X_RANDOM = 10
+const SPEED    = 50
 
-#export(NodePath) var TargetPath = "../../rail/ship"
+export(NodePath) var TargetPath = "../../rail/ship"
 var RailPath                    = "../../rail"
 var target                      = null
 
-var points    = 1
-export var speed_x = 30.0
+var direction = Vector2(0,0)
 var destroyed = false
-var reversed  = false
+
+var target_pos = Vector2(0, 0)
 
 
 func _process(delta):
-	translate(Vector2(speed_x, 0)*delta)
+	global_translate(direction*SPEED*delta)
+
+
+func _ready():
+	target = get_node(TargetPath)
 
 
 func destroy():
@@ -35,11 +38,23 @@ func destroy():
 
 
 func _on_visibility_enter_screen():
-	set_process(true)
+	var rail_speed = target.get_parent().motion
+	var player_speed = target.motion * target.SPEED
+	var player_pos = target.get_global_pos()
+	var distance = (player_pos - get_global_pos()).length() # distance: us from player
+	target_pos = player_pos + rail_speed + Vector2(player_speed.x, 0) * get_process_delta_time() * distance
+	direction = (target_pos - get_global_pos()).normalized()
+#	print("player pos: " + var2str(player_pos))
+#	print("future player pos: " + var2str(target_pos))
+#	print(get_name() + "'s direction: " + var2str(direction))
+	
+	set_process(true) # begin moving
 
 
 func _on_visibility_exit_screen():
-	if get_pos().x < 8:
-		set_pos(Vector2(256,get_pos().y))
-	elif get_pos().x > 256-8:
-		set_pos(Vector2(0,get_pos().y))
+	queue_free()
+
+
+func _on_asteroid_area_enter( area ):
+	if area.is_in_group("Player"):
+		destroy()
