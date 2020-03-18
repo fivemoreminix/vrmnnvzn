49a5870821959249e7c52ffc8ec72f3934a8fe4a
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-17 21:05:20 -0500

    Laser charging animation

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -5,11 +5,14 @@ func _ready():
 #	get_node("TimerWhileOff").start()
 	_on_TimerWhileOn_timeout() # Disable collisions and start TimerWhileOff
 
+
 func expand_beam():
 	var scale = Vector2(get_node("RightHead").get_pos().x-5, 1)
 	get_node("LeftHead/Beam/Sprite").set_scale(scale)
 	get_node("LeftHead/Beam/CollisionShape2D").set_scale(scale)
 	get_node("LeftHead/Beam/CollisionShape2D").set_pos(Vector2(scale.x/2, scale.y)) # colliders scale from middle, so we offset it here
+	
+	get_node("ChargeTex").set_size(scale)
 
 
 func _on_VisibilityEnabler2D_exit_screen():
@@ -23,6 +26,8 @@ func _on_TimerWhileOff_timeout(): # After being off ...
 	
 	# TODO: anim...
 	get_node("TimerWhileCharging").start()
+	get_node("AnimationPlayer").play("Charging") # Start animation
+
 
 func _on_TimerWhileCharging_timeout(): # After fully charging ...
 	# show beam
@@ -32,6 +37,8 @@ func _on_TimerWhileCharging_timeout(): # After fully charging ...
 	get_node("LeftHead/Beam").set_monitorable(true)
 	
 	get_node("TimerWhileOn").start()
+	get_node("AnimationPlayer").stop() # Stop animation
+
 
 func _on_TimerWhileOn_timeout(): # After being on ...
 	# stop showing the beam
