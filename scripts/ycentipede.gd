61974507b7d56512102bcd799b7721d0cfcb257a
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-24 00:28:56 -0500

    YCentipede's collisions fixed

--- scripts/ycentipede.gd
+++ scripts/ycentipede.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 const SEGMENT_TIME_OFFSET = 5.0
 const SEGMENT_OFFSET = Vector2(0, -10)
@@ -8,6 +8,7 @@ export(float) var speed = 2
 export(int) var body_segments = 5
 
 var segment = preload("res://scenes/centipede_segment.tscn")
+var segment_shape = preload("res://scenes/res/ycentipede_segment_shape.tres")
 
 var destroyed = false
 func destroy(): pass
@@ -20,10 +21,20 @@ func _ready():
 	get_node("head/AnimatedSprite").play("head")
 	parts.append(get_node("head"))
 	
+	# Create a reference-counted new shape resource for the body segments to use as their collision (fits the legs)
+	var body_shape = get_node("head/Collider").get_shape().duplicate() # Duplicate resource
+	body_shape.set_extents(Vector2(16, 8)) # Set the collision box wide enough to include legs on both sides of sprite
+	
 	# Create and initialize body and tail parts
 	for i in range(body_segments+1):
 		var n = segment.instance()
-		n.get_node("AnimatedSprite").play("body" if i < body_segments else "tail")
+		
+		if i < body_segments: # If we're not making the last segment ...
+			n.get_node("AnimatedSprite").play("body")
+			n.get_node("Collider").set_shape(body_shape) # Assign segment's shape to become the body shape resource
+		else:
+			n.get_node("AnimatedSprite").play("tail")
+		
 		add_child(n)
 		n.set_pos(SEGMENT_OFFSET * (i+1)) # Set Y position of segment
 		
