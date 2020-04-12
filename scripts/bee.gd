7c75f0b6e51e8c2e0331d6d342c957de936190a8
Luke I. Wilson <asmoaesl@gmail.com>
2020-04-12 13:54:41 -0500

    Bees show stun

--- scripts/bee.gd
+++ scripts/bee.gd
@@ -14,9 +14,14 @@ var destroyed = false
 
 var target_pos = Vector2(0, 0)
 
+var flashing = false
+var is_white = false
 
 func _process(delta):
-	global_translate(direction*SPEED*delta)
+	if flashing:
+		global_translate(-direction*SPEED*delta*0.5)
+	else:
+		global_translate(direction*SPEED*delta)
 
 
 func _ready():
@@ -41,6 +46,10 @@ func destroy():
 			set_process(false)
 			set_monitorable(false) # Disable the collisions on this Bee
 			get_node("sfx").play("sound_explode")
+		else:
+			flashing = true
+			get_node("StunTimer").start()
+			get_node("FlashTimer").start()
 
 
 func _on_visibility_enter_screen():
@@ -71,3 +80,15 @@ func _on_visibility_exit_screen():
 func _on_asteroid_area_enter( area ):
 	if area.is_in_group("Player"):
 		destroy()
+
+
+func _on_StunTimer_timeout():
+	flashing = false
+	get_node("sprite").play("default")
+	get_node("FlashTimer").stop()
+	is_white = false
+
+
+func _on_FlashTimer_timeout():
+	is_white = not is_white
+	get_node("sprite").play("white" if is_white else "default")
