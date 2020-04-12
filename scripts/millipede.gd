381e8580d952b8425250c32fd1d08ed8a6c896cb
Luke I. Wilson <asmoaesl@gmail.com>
2020-04-11 23:05:34 -0500

    Millipedes change sprites based on movement direction

--- scripts/millipede.gd
+++ scripts/millipede.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-const SEGMENT_TIME_OFFSET = 0.1
+const SEGMENT_TIME_OFFSET = 0.075
 const SEGMENT_OFFSET = Vector2(0, -10)
 
 export(NodePath) var path # Path2D
 #export(float) var sine_magnitude = 15.0
 var speed = 0.5
-export(int) var body_segments = 5
+export(int) var body_segments = 10
 
 var segment = preload("res://scenes/millipede_segment.tscn")
 var segment_shape = preload("res://scenes/res/ycentipede_segment_shape.tres")
@@ -20,10 +20,10 @@ var time = 0.0
 
 func _ready():
 	if get_node(path) == null: printerr(get_name() + ": `path` is null, this node is not going to function properly.")
-	
+	set_process(true)
 	
 	# Initialize head
-	get_node("head/AnimatedSprite").play("head")
+	get_node("head/AnimatedSprite").set_animation("head")
 	parts.append(get_node("head"))
 	
 	# Create a reference-counted new shape resource for the body segments to use as their collision (fits the legs)
@@ -38,7 +38,7 @@ func _ready():
 			n.get_node("AnimatedSprite").play("body")
 			n.get_node("Collider").set_shape(body_shape) # Assign segment's shape to become the body shape resource
 		else:
-			n.get_node("AnimatedSprite").play("tail")
+			n.get_node("AnimatedSprite").set_animation("tail")
 		
 		add_child(n)
 		n.set_pos(SEGMENT_OFFSET * (i+1)) # Set Y position of segment
@@ -57,7 +57,37 @@ func _process(delta):
 	var c = get_node(path).get_curve()
 	for i in range(parts.size()): # For every part ...
 		var p_time = max(0.0, time - SEGMENT_TIME_OFFSET*i)
-		parts[i].set_pos(c.interpolatef(p_time))
+		var old_pos = parts[i].get_pos()
+		var new_pos = c.interpolatef(p_time)
+		var motion = (new_pos - old_pos).normalized()
+		var sprite = parts[i].get_node("AnimatedSprite")
+		if sprite.animation != "body":
+			sprite.frame = get_sprite_idx_for_motion(motion)
+#		else:
+#			sprite.set_flip_v(motion.y >= -1.0 and motion.y < 0.0) # When facing up, flip body segments
+		parts[i].set_pos(new_pos)
+
+
+# Only normalized motions!
+func get_sprite_idx_for_motion(m):
+#	0: (-.25 =..=  .25,   0 =..= 1) Down
+#	1: (-.5   ..  -.25,   0 =..= 1) Lower left
+#	2: (-.5  =..= -1,     0 =..= 1) Left
+#	3: (-1    ..  -.25,  -1 =..  0) Upper left
+#	4: (-.25 =..=  .25,  -1 =..  0) Up
+#	5: ( .25  ..   1,    -1 =..  0) Upper right
+#	6: ( .5  =..=  1,     0 =..= 1) Right
+#	7: ( .25  ..   .5,    0 =..= 1) Lower right
+	if m.y >= -1.0 and m.y < 0.0: # This is upward facing...
+		if   m.x > -1.0 and m.x < -.25: return 3 # Upper left
+		elif m.x >= -.25 and m.x <= .25: return 4 # Up
+		else: return 5 # Upper right
+	else: # This faces downward...
+		if   m.x >= -.25 and m.x <= .25: return 0 # Down
+		elif m.x > -.5 and m.x < -.25: return 1 # Lower left
+		elif m.x < -.5 and m.x > -1.0: return 2 # Left
+		elif m.x >= .5 and m.x <= 1.0: return 6 # Right
+		else: return 7 # Lower right
 
 
 # Begin following the path. This can be connected to a VisibilityNotifier in a level to trigger the millipede.
