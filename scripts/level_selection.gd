5b40472cfa384666e263ae9c75070ba3ce4ff7be
Luke I. Wilson <asmoaesl@gmail.com>
2020-04-11 20:53:29 -0500

    Level selection screen saves current level

--- scripts/level_selection.gd
+++ scripts/level_selection.gd
@@ -25,7 +25,11 @@ func cancel():
 
 
 func start():
-	get_tree().change_scene("res://scenes/levels/lvl" + levels[get_node("ItemList").get_selected_items()[0]][1] + ".tscn")
+	var selected_level_idx = levels[get_node("ItemList").get_selected_items()[0]][1]
+	GameData.load_data()
+	GameData.data.current_level = selected_level_idx
+	GameData.save_data()
+	get_tree().change_scene("res://scenes/levels/lvl" + selected_level_idx + ".tscn")
 
 
 func _on_ItemList_item_activated( _ ):
