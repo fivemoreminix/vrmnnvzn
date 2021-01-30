d4a7806cdcc57de8df03ec5688dd5b8c8a5ca55f
shanks-red <lezharayman@gmail.com>
2021-01-30 14:21:06 +0100

    fixed the difficulty_info

--- scripts/difficulty_info.gd
+++ scripts/difficulty_info.gd
@@ -2,31 +2,34 @@ extends Control
 
 var current_difficulty = "Easy"
 var first_time_play = true
-export var infoEasy = "on Easy, enemies are a bit slower,\n and you can shoot two bullets."
+export var infoEasy = "on Easy, enemies are a bit slower,\n and you can shoot one bullet."
 export var infoNormal = "on Normal, enemies are a bit faster, \n you can shoot one bullet. \n and every other thing is just normal ..."
 
-func _ready():
-	load_difficulty()
-	set_info_txt()
 
 
-
-
-func load_difficulty():
-
-	current_difficulty = String(GameData.data.difficulty)
-	if int(GameData.data.current_section) > 0:
-		first_time_play = false # so that it doesn't popup when you have already played the level (when you have allready a section saved)
 
 func set_info_txt():
 	if first_time_play:
 		if current_difficulty == "Easy":
-			get_node("Popup/Info").set_text(infoEasy)
+			get_node("Popup/Panel/Info").set_text(infoEasy)
 		elif current_difficulty == "Normal":
-			get_node("Popup/Info").set_text(infoNormal)
-		get_node("Popup").popup()
+			get_node("Popup/Panel/Info").set_text(infoNormal)
+		show()
 
 
-func _on_infoTime_timeout():
 
-	get_node("Popup").hide()
+func _on_EasyCheck_pressed():
+	current_difficulty = "Easy"
+	set_info_txt()
+
+func _on_NormalCheck_pressed():
+	current_difficulty = "Normal"
+	set_info_txt()
+
+
+func _on_Ok_pressed():
+	hide()
+
+
+func _on_delay_show_timeout():
+	set_info_txt()
