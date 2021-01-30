4f05440e352aab08a627895cf34c91f43dfcc768
shanks-red <lezharayman@gmail.com>
2021-01-30 14:39:21 +0100

    removed some unneeded vars

--- scripts/difficulty_info.gd
+++ scripts/difficulty_info.gd
@@ -1,20 +1,18 @@
 extends Control
 
 var current_difficulty = "Easy"
-var first_time_play = true
 export var infoEasy = "on Easy, enemies are a bit slower,\n and you can shoot one bullet."
 export var infoNormal = "on Normal, enemies are a bit faster, \n you can shoot one bullet. \n and every other thing is just normal ..."
 
 
 
-
 func set_info_txt():
-	if first_time_play:
-		if current_difficulty == "Easy":
-			get_node("Popup/Panel/Info").set_text(infoEasy)
-		elif current_difficulty == "Normal":
-			get_node("Popup/Panel/Info").set_text(infoNormal)
-		show()
+
+	if current_difficulty == "Easy":
+		get_node("Popup/Panel/Info").set_text(infoEasy)
+	elif current_difficulty == "Normal":
+		get_node("Popup/Panel/Info").set_text(infoNormal)
+	show()
 
 
 
